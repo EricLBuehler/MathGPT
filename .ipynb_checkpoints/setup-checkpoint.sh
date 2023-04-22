673f43be4874db8fb6b7bcecc275ee7b181d8da4
EricLBuehler <ericlbuehler@gmail.com>
2023-04-22 20:30:40 +0000

    Update setup

--- .ipynb_checkpoints/setup-checkpoint.sh
+++ .ipynb_checkpoints/setup-checkpoint.sh
@@ -1,3 +1,8 @@
-git clone https://github.com/EricLBuehler/MyGPT.git
-cd MyGPT
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install git-lfs
+git lfs install
+git lfs track "models"
+git clone https://github.com/EricLBuehler/MathGPT.git
+cd MathGPT
 git config --global credential.helper store
--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 sudo apt-get update
 sudo apt-get upgrade
-git clone https://github.com/EricLBuehler/MyGPT.git
-cd MyGPT
+sudo apt-get install git-lfs
+git lfs install
+git lfs track "models"
+git clone https://github.com/EricLBuehler/MathGPT.git
+cd MathGPT
 git config --global credential.helper store
